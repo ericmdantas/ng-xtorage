2c5a38f1ec0e20a3bfc5c3305719e7270e14b404
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-02-11 22:27:20 -0200

    switching from grunt to gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var coveralls = require('gulp-coveralls');
+var rename = require('gulp-rename');
+var karma = require('karma').server;
+
+var _coverage = './coverage/**/lcov.info';
+var _ngXtorage = 'ng-xtorage.js';
+var _ngXtorageMin = 'ng-xtorage.min.js';
+
+gulp.task('build', ['unit_test'], function()
+{
+    gulp
+        .src(_coverage)
+        .pipe(coveralls());
+
+    gulp
+        .src(_ngXtorage)
+        .pipe(uglify())
+        .pipe(rename(_ngXtorageMin))
+        .pipe(gulp.dest('.'))
+})
+
+gulp.task('unit_test', function(done)
+{
+    karma.start({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true,
+        browsers: ['PhantomJS']
+    }, done);
+})
